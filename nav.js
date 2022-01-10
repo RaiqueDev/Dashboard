8e5075a41e7dadd607b28823645422adeddf3d37
Raique Ramos <raiqueramos1@gmail.com>
2022-01-10 10:48:19 -0300

    add nav.js

---
+++ nav.js
@@ -0,0 +1,198 @@
+function dashboardOn() {
+  // ADD
+  document.querySelector('.dashboard').classList.add('dashboardClick');
+  document.querySelector('.dashboard').style.background = "#1d1e2c";
+
+  //REMOVE
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function profileOn() {
+  // ADD
+  document.querySelector('.Userprofiles').classList.add('UserprofilesClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function messagesOn() {
+  // ADD
+  document.querySelector('.Messages').classList.add('MessagesClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function TransactionsOn() {
+  // ADD
+  document.querySelector('.Transactions').classList.add('TransactionsClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function MywalletOn() {
+  // ADD
+  document.querySelector('.Mywallet').classList.add('MywalletClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function PaymentOn() {
+  // ADD
+  document.querySelector('.Payment').classList.add('PaymentClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function InvestmentOn() {
+  // ADD
+  document.querySelector('.Investment').classList.add('InvestmentClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function ReportsOn() {
+  // ADD
+  document.querySelector('.Reports').classList.add('ReportsClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function SettingsOn() {
+  // ADD
+  document.querySelector('.Settings').classList.add('SettingsClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+  
+}
+
+function SupportOn() {
+  // ADD
+  document.querySelector('.Support').classList.add('SupportClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Logout').classList.remove('LogoutClick');
+}
+
+function LogoutOn() {
+  // ADD
+  document.querySelector('.Logout').classList.add('LogoutClick');
+
+  // REMOVE
+  document.querySelector('.dashboard').style.background = "rgba(0, 0, 0, 0)"
+  document.querySelector('.Userprofiles').classList.remove('UserprofilesClick');
+  document.querySelector('.dashboard').classList.remove('dashboardClick');
+  document.querySelector('.Messages').classList.remove('MessagesClick');
+  document.querySelector('.Transactions').classList.remove('TransactionsClick');
+  document.querySelector('.Payment').classList.remove('PaymentClick');
+  document.querySelector('.Mywallet').classList.remove('MywalletClick');
+  document.querySelector('.Investment').classList.remove('InvestmentClick');
+  document.querySelector('.Reports').classList.remove('ReportsClick');
+  document.querySelector('.Settings').classList.remove('SettingsClick');
+  document.querySelector('.Support').classList.remove('SupportClick');
+}
